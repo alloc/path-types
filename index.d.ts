cfd793fa06d1c8d1a326d547152ee452e0af4133
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2024-10-09 14:03:30 -0400

    chore: format

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,12 @@
 type Simplify<T> = {} & { [K in keyof T]: T[K] }
 
 /** Extract param types from a route path literal. */
-export type InferParams<P extends string, Value = string> =
-  InferParamFromPath<P> extends ParamNames<infer Required, infer Optional>
-    ? Simplify<{ [K in Required]: Value } & { [K in Optional]?: Value }>
-    : never
+export type InferParams<
+  P extends string,
+  Value = string
+> = InferParamFromPath<P> extends ParamNames<infer Required, infer Optional>
+  ? Simplify<{ [K in Required]: Value } & { [K in Optional]?: Value }>
+  : never
 
 /** Convert a route path literal to a template type. */
 export type PathTemplate<P extends string> = P extends any
@@ -16,17 +18,17 @@ export type PathTemplate<P extends string> = P extends any
 type InferParamFromPath<P extends string> = P extends `${infer A}/${infer B}`
   ? InferParam<A, InferParamFromPath<B>>
   : P extends `${infer A}&${infer B}`
-    ? InferParam<A, InferParamFromPath<B>>
-    : InferParam<P, { required: never; optional: never }>
+  ? InferParam<A, InferParamFromPath<B>>
+  : InferParam<P, { required: never; optional: never }>
 
 type InferParam<
   T extends string,
-  Acc extends ParamNames,
+  Acc extends ParamNames
 > = T extends `:${infer P}${'?' | '*'}`
   ? AddOptionalParam<Acc, P>
   : T extends `:${infer P}${'+' | ''}`
-    ? AddRequiredParam<Acc, P>
-    : Acc
+  ? AddRequiredParam<Acc, P>
+  : Acc
 
 type AddOptionalParam<PG extends ParamNames, P extends string> = ParamNames<
   PG['required'],
@@ -44,16 +46,16 @@ interface ParamNames<R extends string = string, O extends string = string> {
 
 type PathTemplatePart<
   Part extends string,
-  Rest extends string = never,
+  Rest extends string = never
 > = Part extends `:${string}${'?' | '*'}`
   ? Rest | PathConcat<string, Rest>
   : Part extends `:${string}${'+' | ''}`
-    ? PathConcat<string, Rest>
-    : PathConcat<Part, Rest>
+  ? PathConcat<string, Rest>
+  : PathConcat<Part, Rest>
 
 type PathConcat<Left extends string, Right extends string> = unknown &
   ([Left] extends [never]
     ? Right
     : [Right] extends [never]
-      ? Left
-      : `${Left}/${Right}`)
+    ? Left
+    : `${Left}/${Right}`)
